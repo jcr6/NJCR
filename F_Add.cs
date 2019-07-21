d51f8a032d337f74031345f8954f24fbd9fbbc73
Jeroen P. Broks <facebook@tbbs.nl>
2019-07-21 14:08:59 +0200

    Add

---
+++ F_Add.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrickyUnits;
+
+namespace NJCR {
+    class F_Add : FeatBase {
+
+        public F_Add() {
+            Description = "Creates/Updates JCR file and add/replace files";
+        }
+
+        public override void Run(FlagParse fp) {
+            throw new NotImplementedException();
+        }
+
+        public override void Parse(FlagParse fp) {
+            throw new NotImplementedException();
+        }
+    }
+}
