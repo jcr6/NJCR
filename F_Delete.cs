4bca53d9bfc35b314922f28901a22f19da26fbf3
Jeroen P. Broks <facebook@tbbs.nl>
2019-07-22 12:15:37 +0200

    Delete skeleton

---
+++ F_Delete.cs
@@ -0,0 +1,50 @@
+// Lic:
+// NJCR
+// Remove files from JCR
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 19.07.22
+// EndLic
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrickyUnits;
+
+namespace NJCR {
+    class F_Delete : FeatBase {
+
+        public F_Delete() { Description = "Deletes entries from a JCR file"; }
+
+        public override void Parse(FlagParse fp) {
+            throw new NotImplementedException();
+        }
+
+        public override void Run(FlagParse fp) {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
