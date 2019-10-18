5ad200f4593a7a996ec1e336fe5ac0a5123267b9
Jeroen P. Broks <facebook@tbbs.nl>
2019-10-18 22:58:42 +0200

    vUpdate

--- F_HEX.cs
+++ F_HEX.cs
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 19.10.04
+// Version: 19.10.18
 // EndLic
 using System;
 using TrickyUnits;
@@ -96,5 +96,6 @@ namespace NJCR {
         }
     }
 }
+
 
 
--- F_Verbose.cs
+++ F_Verbose.cs
@@ -201,3 +201,4 @@ namespace NJCR {
     }
 }
 
+
