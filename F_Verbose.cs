94e9e4c555a38367e6c12c2f7d4e0547fce4b953
Jeroen P. Broks <facebook@tbbs.nl>
2019-10-18 21:21:06 +0200

    Actual verbose, but I'm not ready yet!

--- F_Verbose.cs
+++ F_Verbose.cs
@@ -9,8 +9,29 @@ namespace NJCR {
     class F_Verbose : FeatBase {
 
         enum Just { Left, Right }
+        TMap<string, string> FTypes = new TMap<string, string>();
 
-        public F_Verbose() { Description = "Verboses content of a JCR resource"; }
+        public F_Verbose() {
+            Description = "Verboses content of a JCR resource";
+            FTypes["png"] = "Image";
+            FTypes["jpg"] = "Image";
+            FTypes["jpeg"] = "Image";
+            FTypes["gif"] = "Image";
+            FTypes["txt"] = "Text";
+            FTypes["md"] = "Markdown";
+            FTypes["html"] = "HTML file";
+            FTypes["htm"] = "HTML file";
+            FTypes["js"] = "Script";
+            FTypes["saskia"] = "Script";
+            FTypes["lua"] = "Script";
+            FTypes["nil"] = "Script";
+            FTypes["py"] = "Script";
+            FTypes["wav"] = "Audio";
+            FTypes["mp3"] = "Audio";
+            FTypes["ogg"] = "Audio";
+            FTypes["gini"] = "Config";
+            FTypes["ini"] = "Config";
+        }
 
         public override void Parse(FlagParse fp) {
             fp.CrBool("x", false); // so extended stuff too like Author and Notes
@@ -67,31 +88,62 @@ namespace NJCR {
                     Console.WriteLine();
                 }
             }
+            // Comments
             foreach(string name in jcr.Comments.Keys) {
                 QCol.White($"{name}\n");
                 for (int i = 0; i < name.Length; i++) QCol.White("=");
                 QCol.Yellow($"\n{jcr.Comments[name]}\n\n");
             }
 
+            XPrint(15, ConsoleColor.White, "Kind"); WhiteSpace(2);
+            XPrint(10, ConsoleColor.White, "Compressed", Just.Right); WhiteSpace(2);
+            XPrint(10, ConsoleColor.White, "Size", Just.Right); WhiteSpace(2);
+            XPrint(5, ConsoleColor.White, "Ratio", Just.Right); WhiteSpace(2);
+            XPrint(7, ConsoleColor.White, "Storage"); WhiteSpace(2);
+            Console.WriteLine("Entry");
+            XPrint(15, ConsoleColor.White, "===="); WhiteSpace(2);
+            XPrint(10, ConsoleColor.White, "==========", Just.Right); WhiteSpace(2);
+            XPrint(10, ConsoleColor.White, "====", Just.Right); WhiteSpace(2);
+            XPrint(5, ConsoleColor.White, "=====", Just.Right); WhiteSpace(2);
+            XPrint(7, ConsoleColor.White, "======="); WhiteSpace(2);
+            Console.WriteLine("=====");
+            foreach (TJCREntry ent in jcr.Entries.Values) {
+                Console.BackgroundColor = ConsoleColor.Black;
+                if (ent.MainFile != fp.Args                    [1].Replace("\\", "/")) Console.BackgroundColor = ConsoleColor.DarkBlue;
+                XPrint(15, ConsoleColor.Blue, FTypes[qstr.ExtractExt(ent.Entry).ToLower()]); WhiteSpace(2);
+                XPrint(10, ConsoleColor.Green, ent.CompressedSize); WhiteSpace(2);
+                XPrint(10, ConsoleColor.Red, ent.Size); WhiteSpace(2);
+                XPrint(5, ConsoleColor.Magenta, ent.Ratio, Just.Right); WhiteSpace(2);
+                XPrint(7, ConsoleColor.Yellow, ent.Storage); WhiteSpace(2);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(ent.Entry);
+            }
         }
 
         void XPrint(int size, ConsoleColor col, string text, Just align = Just.Left) {
-            var x = Console.CursorLeft;
-            Console.ForegroundColor = col;
-            if (text.Length > size) {
-                Console.WriteLine(text);
-                while (Console.CursorLeft < x + size) Console.Write(" ");
-            } else {
-                switch (align) {
-                    case Just.Left:
-                        Console.Write(text);
-                        for (int i = text.Length; i < size; i++) Console.Write(" ");
-                        break;
-                    case Just.Right:
-                        for (int i = 0; i< size-text.Length;  i++) Console.Write(" ");
-                        Console.Write(text);
-                        break;
+            try {
+                if (text == null) text = "";
+                var x = Console.CursorLeft;
+                Console.ForegroundColor = col;
+                if (text.Length > size) {
+                    Console.WriteLine(text);
+                    while (Console.CursorLeft < x + size) Console.Write(" ");
+                } else {
+                    switch (align) {
+                        case Just.Left:
+                            Console.Write(text);
+                            for (int i = text.Length; i < size; i++) Console.Write(" ");
+                            break;
+                        case Just.Right:
+                            for (int i = 0; i < size - text.Length; i++) Console.Write(" ");
+                            Console.Write(text);
+                            break;
+                    }
                 }
+            } catch (Exception crap) {
+                QCol.QuickError(crap.Message);
+                QCol.Magenta(crap.StackTrace);
+                Console.Write("\n\n");
             }
         }
         void XPrint(int size, ConsoleColor col, int whatever, Just align = Just.Right) => XPrint(size, col, $"{whatever}", align);
