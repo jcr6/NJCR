541a7b38e5a1ecd8668d29b8db541859c596cc5c
Jeroen P. Broks <facebook@tbbs.nl>
2019-10-19 23:12:50 +0200

    Ask

--- F_Show.cs
+++ F_Show.cs
@@ -26,7 +26,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
 using UseJCR6;
 using TrickyUnits;
@@ -35,25 +35,39 @@ namespace NJCR {
     class F_Show:FeatBase {
 
         readonly string ShowConfig = Dirry.C("$AppSupport$/NJCR_Show.GINI");
+        readonly TGINI Config;
+
+        public override void Parse(FlagParse fp) {                    }
 
         public override void Run(FlagParse fp) {
             if (fp.Args.Length < 2) {
                 QCol.Green("This feature allows you to show the content of a certain file.\n");
                 QCol.Green("In order to do this, some additional configuration will be needed, as well as knowledge of the file system you are using.\n");
                 QCol.Green("Don't worry about having to set up this file, manually. You will be prompted whenever NJCR requires more information.\n");
-                QCol.Green($"The configuration can lateron be edited with your favority text editor, as it will be saved as {ShowConfig}");
+                QCol.Green($"The configuration can lateron be edited with your favority text editor (as long as it supports Unix LF line ends), as it will be saved as {ShowConfig}");
                 return;
             }
             var jcr = JCR6.Dir(fp.Args[1]);
             if (jcr==null) { QCol.QuickError(JCR6.JERROR); return; }
+            if (!File.Exists(ShowConfig)) QuickStream.SaveString(ShowConfig, "[rem]\nNothint to see here yet\n");
             for (int i = 2; i < fp.Args.Length; i++) Show(jcr, fp.Args[i]);
         }
 
-
-        public override void Parse(FlagParse fp) {            
+        void Ask(string Tag, string Uitleg,string Vraag) {
+            if (Config.C(Tag) != "") return;
+            QCol.Green($"{Uitleg}\n");
+            var Antwoord = "";
+            do {
+                QCol.Yellow($"{Vraag}: ");
+                Antwoord = Console.ReadLine().Replace("\\", "/").Trim();
+            } while (Antwoord == "");
+            Config.D(Tag, Antwoord);
+            Config.SaveSource(ShowConfig);
         }
 
-        void Show(TJCRDIR jcr,string entry) { }
+
+        void Show(TJCRDIR jcr,string entry) {
+        }
     }
 }
 
