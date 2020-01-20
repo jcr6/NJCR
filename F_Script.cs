62527050a498fe2e31e4b02f02cd067069ef86a7
Jeroen P. Broks <facebook@tbbs.nl>
2020-01-20 20:32:15 +0100

    Script core

---
+++ F_Script.cs
@@ -0,0 +1,184 @@
+// Lic:
+// NJCR
+// Scripting engine
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 19.12.26
+// EndLic
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using TrickyUnits;
+using NLua;
+
+namespace NJCR {
+
+    class ScriptAPI {
+
+        static F_Add Adder => F_Add.X;
+        public bool Verbose = false;
+
+        void V(string m) { if (Verbose) QCol.Magenta($"{m}\n"); }
+
+        public int ArgCount => F_Script.ScriptArgs.Length;
+        public string Arg(int i) {
+            if (i < 0 || i >= ArgCount) throw new Exception("Invalid arg index");
+            return F_Script.ScriptArgs[i];
+        }
+
+        public string OutputFile { set { F_Script.JLS_Data_Output = value; Adder.jcrfile = value; } get => F_Script.JLS_Data_Output; }
+        public void Output(string arg) {
+            var s = arg.Split(':');
+            if (s.Length != 2) throw new Exception("Invalid JLS.Output string");
+            F_Script.JLS_Data[s[0]] = s[1];
+        }
+
+        private TMap<string, string> AddMap = null;
+        public void SetAdd(string key,string value) {
+            try {
+                if (key != "Start" && AddMap == null) throw new Exception("Cannot add data to add request when no request has been opened!");
+                switch (key) {
+                    case "Start":
+                        AddMap = new TMap<string, string>();
+                        break;
+                    case "Close":
+                        if (AddMap["Target"] == "") AddMap["Target"] = qstr.StripDir(AddMap["Source"]);
+                        if (AddMap["Source"] == "") throw new Exception("Cannot add without source");
+                        if (AddMap["Storage"] == "") AddMap["Storage"] = "Store";
+                        if (!UseJCR6.JCR6.CompDrivers.ContainsKey(AddMap["Storage"])) throw new Exception($"Unknown compression method requested {AddMap["Storage"]}");
+                        if (File.Exists(AddMap["Source"])) {
+                            V($"F:Adding file: {AddMap["Source"]} as {AddMap["Target"]}");
+                            Adder.Add2List(AddMap["Source"], AddMap["Target"], AddMap["Storage"], AddMap["Author"], AddMap["Notes"]);
+                        } else if (Directory.Exists(AddMap["Source"])) {
+                            var list = FileList.GetTree(AddMap["Source"]);
+                            foreach (string f in list) {
+                                V($"D:Adding file: {f} from directory {AddMap["Source"].Replace('\\', '/')} => {AddMap["Target"]}");
+                                Adder.Add2List($"{AddMap["Source"].Replace('\\', '/')}/{f}", $"{AddMap["Target"]}/{f}", AddMap["Storage"], AddMap["Author"], AddMap["Notes"]);
+                            }
+                        } else {
+                            throw new Exception($"Source {AddMap["Source"]} not found!");
+                        }
+                        AddMap = null;
+                        break;
+                    default:
+                        AddMap[key] = value;
+                        break;
+                }
+            } catch (Exception e) {
+                QCol.QuickError($"{e.Message}");
+                F_Script.State.DoString("print(debug.traceback())");
+                throw new Exception("Error");
+            }
+        }
+
+        public void AddAlias(string ori, string target) {
+            V($"Alias: {ori} => {target}");
+            Adder.AddAlias(ori, target);
+        }
+
+        public void AddComment(string name, string content) => Adder.AddComment(name, content);
+        
+        public string GetDir(string dir) {
+            var r = new StringBuilder();
+            var gd = FileList.GetTree(dir);
+            foreach(string f in gd) {
+                if (r.Length > 0) r.Append(", ");
+                r.Append($"\"{f}\"");
+            }
+            return r.ToString();
+        }
+
+    }
+
+    class F_Script : FeatBase {
+
+        static F_Add Adder => F_Add.X;
+
+        static internal string[] ScriptArgs;
+        static internal string ScriptFile;
+        static internal string Script;
+        static internal string ScriptType;        
+        static internal Lua State;
+
+
+        static public string JLS_Data_Output = "";
+        static public TMap<string, string> JLS_Data = new TMap<string, string>();
+
+
+        public F_Script() {
+            Description = "Creates JCR file based on script files written in either Lua or NIL";
+        }
+
+
+        public override void Run(FlagParse fp) {
+            if (fp.Args.Length==1) {
+                QCol.Doing("Usage", "NJCR script <script> [args]");
+                return;
+            }
+            ScriptFile = fp.Args[1];
+            ScriptType = qstr.ExtractExt(ScriptFile).ToUpper();
+            if (fp.Args.Length == 2)
+                ScriptArgs = new string[0];
+            else {
+                ScriptArgs = new string[fp.Args.Length - 2];
+                for(int i = 1; i < fp.Args.Length - 2; i++) {
+                    ScriptArgs[i] = fp.Args[i + 2];
+                }
+            }
+            try {
+                QCol.Doing("Loading", ScriptFile);
+                QCol.Doing("Type", ScriptType);
+                Script = QuickStream.LoadString(ScriptFile);
+                QCol.Doing("Configuring", "Default Data");
+                JLS_Data_Output = $"{qstr.StripExt(ScriptFile)}.JCR";
+                QCol.Doing("Creating", "Work state");
+                State = new Lua();
+                State["__NJCR"] = new ScriptAPI();
+                QCol.Doing("Compiling", "Base Script");
+                var BaseScript = QuickStream.StringFromEmbed("BaseScript.lua");
+                State.DoString(BaseScript, "NJCR Base Script");
+                QCol.Doing("Compiling", ScriptFile);
+                switch (ScriptType) {
+                    case "LUA":
+                        State.DoString(Script, $"LUA:{qstr.StripDir(ScriptFile)}");
+                        break;
+                    case "NIL":
+                        throw new Exception("NIL support has not yet been implemented. Please wait awhile");
+                    default:
+                        throw new Exception($"Unknown script type: {ScriptType}");
+                }
+                QCol.Doing("Starting", "Packer");
+                QCol.Doing("Creating", Adder.jcrfile);
+                Adder.updating = false;
+                Adder.Go();
+            } catch (Exception e) {
+                QCol.QuickError(e.Message);                
+                return;
+            }
+        }
+
+        public override void Parse(FlagParse fp) {            
+        }
+
+    }
+}
---
+++ Script/BaseScript.lua
@@ -0,0 +1,87 @@
+--[[
+Script/BaseScript.lua
+Base Script for NJCR scripting engine
+version: 19.12.26
+Copyright (C)  Jeroen P. Broks
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+1. The origin of this software must not be misrepresented; you must not
+claim that you wrote the original software. If you use this software
+in a product, an acknowledgment in the product documentation would be
+appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+]]
+
+local NJCR = __NJCR __NJCR = nil -- Security measure!
+local trueJLS = {}
+local metaJLS = {}
+
+function metaJLS.__index(s,key)
+	assert(trueJLS[key],"There is no property/method/field called "..key.." in JLS");
+	return trueJLS[key]
+end
+
+function metaJLS.__newindex(s,key,value)
+	assert(trueJLS["Accept"..key],"Can't write to key '"..key.."'. Either it doesn't exist, or is read-only!")
+	trueJLS["Accept"..key](value)
+end
+
+function trueJLS.SetJCR6OutputFile(value)
+	NJCR.OutputFile = tostring(value)
+end
+
+trueJLS.AcceptSetJCR6OutputFile = trueJLS.Output
+
+function trueJLS.Output(value)
+	NJCR:Output(value)
+end
+
+function trueJLS.Add(source,target,data)
+	NJCR:SetAdd("Start","Let's go!") -- The 2nd parameter just had to be set, but has no meaning, whatsover!
+	NJCR:SetAdd("Source",source)
+	NJCR:SetAdd("Target",target)
+	for k,v in pairs(data) do
+		-- Please note that the system will simply ignore fields it doesn't know. This to prevent crashes with newer versions where more support is given (which is not deemed likely at the present time)
+		NJCR:SetAdd(k,v)
+	end
+	NJCR:SetAdd("Close","Goodbye")
+end
+
+function trueJLS.Alias(ori,target)
+	NJCR:AddAlias(ori,target)
+end
+
+function trueJLS.AcceptVerbose(v)
+	NJCR.Verbose = v~=nil and v~=false
+	trueJLS.Verbose = NJCR.Verbose
+end
+trueJLS.Verbose = NJCR.Verbose
+
+function trueJLS.AddComment(n,c)
+	assert(type(n)=="string","Comment name error!")
+	assert(type(c)=="string","Comment content error!")
+	NJCR:AddComment(n,c)
+end
+
+function trueJLS.GetDir(s)
+	local s = NJCR:GetDir(s)
+	local f = load("return {"..s.."}","GetDir Query")
+	assert(f,"Get Dir not wll received!\n\n"..s)
+	local ret = f()
+	assert(ret,"Get Dir ould not generate correct output\n\n"..s)
+	return ret
+end
+
+
+JLS = setmetatable({},metaJLS)
+
+Add = JLS.Add
+Alias = JLS.Alias
+AddComment = JLS.AddComment
+GetDir = JLS.GetDir
