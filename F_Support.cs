99897fa9f1d7c6f9eb483e6473f50705912fea9c
Jeroen P. Broks <facebook@tbbs.nl>
2020-01-20 20:32:46 +0100

    Support list

---
+++ F_Support.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UseJCR6;
+using TrickyUnits;
+
+namespace NJCR {
+    class F_Support:FeatBase {
+        public F_Support() {
+            Description = "List of all supported file types and compression algorithms";
+        }
+
+        public override void Parse(FlagParse fp) {
+            
+        }
+
+        public override void Run(FlagParse fp) {
+            QCol.Magenta("Supported compression algoritms\n");
+            foreach (string k in JCR6.CompDrivers.Keys) {
+                QCol.Red("= ");
+                QCol.Cyan($"{k}\n");
+            }
+            QCol.Magenta("Resource file readers supported\n");
+            foreach (string k in JCR6.FileDrivers.Keys) {
+                QCol.Red("= ");
+                QCol.Cyan($"{k}\n");
+            }
+        }
+
+    }
+}
