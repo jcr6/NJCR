d736b1bbeaf6b651f7d3b50738a0ab7f180ba095
Jeroen Broks <facebook@tbbs.nl>
2020-11-10 01:48:20 +0100

    Dupe lic block removed

--- Script/BaseScript.lua
+++ Script/BaseScript.lua
@@ -2,7 +2,7 @@
 -- Script/BaseScript.lua
 -- Base Script for NJCR scripting engine
 -- version: 20.11.10
--- Copyright (C) 2019 Jeroen P. Broks
+-- Copyright (C) 2019, 2020 Jeroen P. Broks
 -- This software is provided 'as-is', without any express or implied
 -- warranty.  In no event will the authors be held liable for any damages
 -- arising from the use of this software.
@@ -17,25 +17,6 @@
 -- misrepresented as being the original software.
 -- 3. This notice may not be removed or altered from any source distribution.
 -- </License Block>
---[[
-Script/BaseScript.lua
-Base Script for NJCR scripting engine
-version: 19.12.26
-Copyright (C)  Jeroen P. Broks
-This software is provided 'as-is', without any express or implied
-warranty.  In no event will the authors be held liable for any damages
-arising from the use of this software.
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
-1. The origin of this software must not be misrepresented; you must not
-claim that you wrote the original software. If you use this software
-in a product, an acknowledgment in the product documentation would be
-appreciated but is not required.
-2. Altered source versions must be plainly marked as such, and must not be
-misrepresented as being the original software.
-3. This notice may not be removed or altered from any source distribution.
-]]
 
 local NJCR = __NJCR __NJCR = nil -- Security measure!
 local trueJLS = {}
